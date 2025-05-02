a215e606e2d8291716b4275041caca9a05d97a43
Michael <zankin1@gmail.com>
2025-05-02 14:29:49 +0200

    added one more reservation for test purposes

--- src/Booking.java
+++ src/Booking.java
@@ -17,9 +17,10 @@ public class Booking {
 
         Reservations reservations = new Reservations();
 
-        reservations.addReservation(guest1, room1, LocalDate.of(2021, 7, 19), LocalDate.of(2021, 7, 26));
-        reservations.addReservation(guest2, room3, LocalDate.of(2021, 9, 1), LocalDate.of(2021, 9, 14));
-        reservations.addReservation(guest1, room3, LocalDate.of(2021, 9, 1), LocalDate.of(2021, 9, 14));
+        reservations.addReservation(guest1, room1, LocalDate.of(2021,7,19),LocalDate.of(2021,7,26));
+        reservations.addReservation(guest2, room3, LocalDate.of(2021,9,1),LocalDate.of(2021,9,14));
+        reservations.addReservation(guest1, room3, LocalDate.of(2021,9,1),LocalDate.of(2021,9,14));
+        reservations.addReservation(guest2, room1, LocalDate.of(2021,9,15),LocalDate.of(2021,9,29));
        
         reservations.printReservations();
 
